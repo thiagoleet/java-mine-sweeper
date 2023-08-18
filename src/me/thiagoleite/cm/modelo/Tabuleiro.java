0f7a933960021258413675dfa04cd51f600ff601
Thiago Leite <thiagofmleite@gmail.com>
2023-08-18 13:57:00 -0300

    Montando o tabuleiro

--- src/me/thiagoleite/cm/modelo/Tabuleiro.java
+++ src/me/thiagoleite/cm/modelo/Tabuleiro.java
@@ -1,5 +1,41 @@
 package me.thiagoleite.cm.modelo;
 
-public class Tabuleiro {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Tabuleiro {
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    private final List<Campo> campos = new ArrayList<Campo>();
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarOsVizinhos();
+        sortearMinas();
+    }
+
+    private void sortearMinas() {
+    }
+
+    private void gerarCampos() {
+        for (int linha = 0; linha < linhas; linha++) {
+            for (int coluna = 0; coluna < colunas; coluna++) {
+                campos.add(new Campo(linha, coluna));
+            }
+        }
+    }
+
+    private void associarOsVizinhos() {
+        for (Campo c1 : campos) {
+            for (Campo c2 : campos) {
+                c1.adicionarVizinho(c2);
+            }
+        }
+    }
 }
